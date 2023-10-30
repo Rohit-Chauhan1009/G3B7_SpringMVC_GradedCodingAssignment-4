96f1c7845d5246c618172461225331a37ca626a3
Rohit-Chauhan1009 <128196058+Rohit-Chauhan1009@users.noreply.github.com>
2023-10-30 20:48:34 +0530

    Delete EmployeeManagementRestAPI directory

--- EmployeeManagementRestAPI/src/main/java/com/GreatLearning/EmployeeManagementRestAPI/Repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.GreatLearning.EmployeeManagementRestAPI.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.GreatLearning.EmployeeManagementRestAPI.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-}
